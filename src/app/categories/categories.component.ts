03148ed3f878cfe3e9032aaaf117119d249636db
Caroline <wanguic49@gmail.com>
2024-05-02 14:31:43 +0300

    changes made to the stylng

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -3,6 +3,8 @@ import { MatTableModule, MatTable } from '@angular/material/table';
 import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
 import { MatSortModule, MatSort } from '@angular/material/sort';
 import { CategoriesDataSource, CategoriesItem } from './categories-datasource';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-categories',
@@ -14,7 +16,15 @@ import { CategoriesDataSource, CategoriesItem } from './categories-datasource';
     
   `,
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule]
+  imports: [MatTableModule, 
+            MatPaginatorModule, 
+            MatSortModule,
+            MatTableModule,
+            MatPaginatorModule,
+            MatSortModule,
+            MatCardModule,
+            MatButtonModule
+          ]
 })
 export class CategoriesComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
