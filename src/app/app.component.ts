63977957e1dda7d8cf7b59665db0e5f50c174e12
Caroline <wanguic49@gmail.com>
2024-05-02 11:11:54 +0300

    create home home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from "@angular/material/button";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { HomeComponent } from './home/home.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+            CommonModule,
+            MatButtonModule,
+            MatSlideToggleModule,
+            HomeComponent
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-sales-app';
+
 }
